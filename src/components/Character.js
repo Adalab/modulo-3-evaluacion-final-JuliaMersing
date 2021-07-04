82416678611d18f482804f0ccd5eb596f6373bef
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-07-04 18:20:32 +0200

    add bonus

--- src/components/Character.js
+++ src/components/Character.js
@@ -4,6 +4,24 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
 const Character = (props) => {
+  const renderStatus = () => {
+    if (props.character.status === "Dead") {
+      return <i class="fas fa-skull"></i>;
+    } else if (props.character.status === "unknown") {
+      return <i title="Dear or alive...Who knows!" class="fas fa-question"></i>;
+    } else if (props.character.status === "Alive") {
+      return <i class="fas fa-heartbeat"></i>;
+    }
+  };
+
+  const renderSpecies = () => {
+    if (props.character.specie === "Human") {
+      return <i class="far fa-user"></i>;
+    } else if (props.character.specie === "Humanoid") {
+      return <i class="fab fa-reddit-alien"></i>;
+    }
+  };
+
   return (
     <Link className="link character" to={`/character/${props.character.id}`}>
       <div className="character__card">
@@ -16,8 +34,9 @@ const Character = (props) => {
           alt={props.character.name}
         />
         <div className="character__card--info">
-          <p className="character__card--text-info">{props.character.specie}</p>
+          <p className="character__card--text-info">{renderSpecies()}</p>
           <p className="character__card--text-info">{props.character.gender}</p>
+          <p className="character__card--text-info">{renderStatus()}</p>
         </div>
       </div>
     </Link>
@@ -32,3 +51,61 @@ Character.propTypes = {
   status: PropTypes.string,
   species: PropTypes.string,
 };
+
+/* import React from "react";
+import "../stylesheet/Character.scss";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const Character = (props) => {
+  const renderStatus = () => {
+    if (props.character.status === "Dead") {
+      return <i class="fas fa-skull"></i>;
+    } else if (props.character.status === "unknown") {
+      return <i title="Dear or alive...Who knows!" class="fas fa-question"></i>;
+    } else if (props.character.status === "Alive") {
+      return <i class="fas fa-heartbeat"></i>;
+    }
+  };
+
+  const renderSpecies = () => {
+    if (props.character.specie === "Human") {
+      return <i class="far fa-user"></i>;
+    } else if (props.character.specie === "Humanoid") {
+      return <span>👽</span>;
+    } else if (props.character.speci === "unknown") {
+      return <span>⚧️</span>;
+    }
+
+    return (
+      <Link className="link character" to={`/character/${props.character.id}`}>
+        <div className="character__card">
+          <div className="character__card--name">
+            <h4>{props.character.name}</h4>
+          </div>
+          <img
+            className="character__card--img"
+            src={props.character.photo}
+            alt={props.character.name}
+          />
+          <div className="character__card--info">
+            <p className="character__card--text-info">{renderSpecies()}</p>
+            <p className="character__card--text-info">
+              {props.character.gender}
+            </p>
+            <p className="character__card--text-info">{renderStatus()}</p>
+          </div>
+        </div>
+      </Link>
+    );
+  };
+};
+
+export default Character;
+
+Character.propTypes = {
+  name: PropTypes.string,
+  photo: PropTypes.string,
+  status: PropTypes.string,
+  species: PropTypes.string,
+}; */
--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -3,24 +3,6 @@ import { Link } from "react-router-dom";
 import "../stylesheet/CharacterDetails.scss";
 
 const CharacterDetail = (props) => {
-  const renderStatus = () => {
-    if (props.character.status === "Dead") {
-      return <i class="fas fa-dizzy"></i>;
-    } else if (props.character.status === "unknown") {
-      return <i class="far fa-question-circle"></i>;
-    } else if (props.character.status === "Alive") {
-      return "Alive";
-    }
-  };
-
-  const renderSpecies = () => {
-    if (props.character.specie === "Human") {
-      return <i class="far fa-user"></i>;
-    } else if (props.character.specie === "Humanoid") {
-      return <i class="fab fa-reddit-alien"></i>;
-    }
-  };
-
   return (
     <div className="SingleCharacter">
       <article className="singleCharacterCard">
@@ -32,10 +14,10 @@ const CharacterDetail = (props) => {
         <div className="characterDetails">
           <h4>{props.character.name}</h4>
           <ul>
-            <li>Specie: {renderSpecies()}</li>
+            <li>Specie: {props.character.species}</li>
             <li>Planet: {props.character.planet}</li>
             <li>Episodes: {props.character.episode.length}</li>
-            <li>Status: {renderStatus()}</li>
+            <li>Status: {props.character.status}</li>
           </ul>
         </div>
       </article>
